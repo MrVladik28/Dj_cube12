11ff4081dc3c885605da100eee6e65a2a8d4c950
Vlad <polosuhin.vladislaw@yandex.ru>
2023-11-22 19:46:02 +0300

    ФОТООООООdddddfffffff
    орооfffffvvvvvvvvvпипипи

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -12,6 +12,19 @@ class Pupil(models.Model):
         mame = models.CharField(max_length=30)
         patronymic = models.CharField(max_length=30,blank=True)
         age = models.IntegerField(default=10)
-        class_p = models.IntegerField(max_length=30)
+        class_p = models.IntegerField(default=15)
+
+
+class Workers(models.Model):
+    FIO = models.CharField(max_length=30)
+    Home = models.CharField(max_length=30)
+    Date = models.DateTimeField(blank=True)
+    Education = models.CharField(default="Высшее")
+    Language = models.IntegerField(max_length=30, blank=True)
+    Trips = models.BooleanField(defauit=true)
+    Post = models.CharField(max_length=30)
+    Salary = models.IntegerField(default=19000000000000000000000000000000000000000000000000)
+
+
 
 
